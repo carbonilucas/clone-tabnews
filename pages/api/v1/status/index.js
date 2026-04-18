4b0df428fcfea9fc2982399237aec749a419583e
Lucas Carboni <carboni0306@gmail.com>
2026-04-18 19:51:47 -0300

    Improve  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,35 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+
+  const versionResult = await database.query("SHOW server_version;");
+  const databaseVersion = versionResult.rows[0]?.server_version;
+
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = parseInt(
+    maxConnectionsResult.rows[0]?.max_connections,
+    10,
+  );
+
+  // Used connections can be obtained from pg_stat_activity or pg_stat_database
+  const databaseName = process.env.POSTGRES_DB;
+  const usedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_catalog.pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const usedConnections = usedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        database_version: databaseVersion,
+        max_connections: maxConnections,
+        used_connections: usedConnections,
+      },
+    },
+  });
 }
 
 export default status;
