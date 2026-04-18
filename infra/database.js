bb6c348e86e014358c2a7c6a4c07c4463d7e1241
Lucas Carboni <carboni0306@gmail.com>
2026-04-18 19:49:39 -0300

    Make  more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -8,13 +8,18 @@ async function query(queryObj) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-
   await client.connect();
-  const result = await client.query(queryObj);
-  await client.end();
-  return result;
+
+  try {
+    const result = await client.query(queryObj);
+    return result;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
-  query: query,
+  query,
 };
